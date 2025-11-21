499b9b733deed97b7e6d44c86e4a880d57971fb3
Heidi Holappa <heidi.holappa@helsinki.fi>
2025-11-21 18:48:31 +0200

    Add solution to exercise 1.2
    Had quite a struggle with DNS issues

---
+++ ex-1-2/todo.app/src/main/java/com/example/todo/app/eventlistener/ServerPortLogger.java
@@ -0,0 +1,22 @@
+package com.example.todo.app.eventlistener;
+
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.boot.web.context.WebServerApplicationContext;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+class ServerPortLogger {
+
+    private final WebServerApplicationContext context;
+
+    public ServerPortLogger(WebServerApplicationContext context) {
+        this.context = context;
+    }
+
+    @EventListener(ApplicationStartedEvent.class)
+    public void logPort() {
+        int port = context.getWebServer().getPort();
+        System.out.println("Server port: " + port);
+    }
+}
